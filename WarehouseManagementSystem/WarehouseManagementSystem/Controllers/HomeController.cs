8b5dcfa8a9e7817c675c333ec634488038d83d20
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2025-07-03 18:25:26 +0100

    Improved the Admin Home Page

--- WarehouseManagementSystem/WarehouseManagementSystem/Controllers/HomeController.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using WarehouseManagementSystem.Models;
 
 namespace WarehouseManagementSystem.Controllers
@@ -7,16 +8,27 @@ namespace WarehouseManagementSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly WarehouseDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, WarehouseDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var totalProducts = await _context.Products.CountAsync();
+            var totalSuppliers = await _context.Suppliers.CountAsync();
+            var totalCategories = await _context.Categories.CountAsync();
+
+            ViewData["TotalProducts"] = totalProducts;
+            ViewData["TotalSuppliers"] = totalSuppliers;
+            ViewData["TotalCategories"] = totalCategories;
+
             return View();
         }
+
 
         public IActionResult Privacy()
         {
