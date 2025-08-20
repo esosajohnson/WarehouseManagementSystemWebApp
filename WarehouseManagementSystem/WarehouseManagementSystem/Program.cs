3e0859acdc42814ba6198ea126cbd5e5d8c7c0c2
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2025-08-20 23:59:20 +0100

    Enable Identity with Role support for user authorization

--- WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -14,12 +14,46 @@ builder.Services.AddDbContext<WarehouseDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Configure Identity with ApplicationUser
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<WarehouseDbContext>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<WarehouseDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    string[] roleNames = { "Admin", "Client" };
+    foreach (var roleName in roleNames)
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+    }
+
+    string adminEmail = "admin@gmail.com";
+    string adminPassword = "Admin@1234";
+    var adminUser = await userManager.FindByEmailAsync(adminEmail);
+    if (adminUser == null)
+    {
+        adminUser = new ApplicationUser
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            FullName = "Admin User",
+        };
+        var createAdminResult = await userManager.CreateAsync(adminUser, adminPassword);
+        if (createAdminResult.Succeeded)
+        {
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
