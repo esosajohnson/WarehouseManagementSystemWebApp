34a1c7f627dbccae4083c211aafcf8b81c3df56d
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2025-10-08 00:40:13 +0100

    Created the Edit Profile page and amended the Account Overview page

--- WarehouseManagementSystem/WarehouseManagementSystem/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,12 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WarehouseManagementSystem.Models;
@@ -16,91 +8,35 @@ namespace WarehouseManagementSystem.Areas.Identity.Pages.Account.Manage
     public class IndexModel : PageModel
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public IndexModel(
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+        public IndexModel(UserManager<ApplicationUser> userManager)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
         }
-
-        public string Username { get; set; }
 
         [TempData]
         public string StatusMessage { get; set; }
 
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        public class InputModel
-        {
-            [Display(Name = "Full Name")]
-            public string FullName { get; set; }
-
-            [DataType(DataType.Date)]
-            [Display(Name = "Date of Birth")]
-            public DateTime? DateOfBirth { get; set; }
-
-            [Phone]
-            [Display(Name = "Phone Number")]
-            public string PhoneNumber { get; set; }
-        }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound("Unable to load user.");
             }
 
-            Username = user.UserName;
-
-            Input = new InputModel
-            {
-                FullName = user.FullName,
-                DateOfBirth = user.DateOfBirth,
-                PhoneNumber = user.PhoneNumber
-            };
+            FullName = user.FullName;
+            Email = user.Email;
+            PhoneNumber = user.PhoneNumber;
+            var roles = await _userManager.GetRolesAsync(user);
+            Role = string.Join(", ", roles);
 
             return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            // Update user fields
-            user.FullName = Input.FullName;
-            user.DateOfBirth = (DateTime)Input.DateOfBirth;
-            user.PhoneNumber = Input.PhoneNumber;
-
-            var updateResult = await _userManager.UpdateAsync(user);
-
-            if (!updateResult.Succeeded)
-            {
-                foreach (var error in updateResult.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-                return Page();
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Your profile has been updated";
-
-            return RedirectToPage();
         }
     }
 }
