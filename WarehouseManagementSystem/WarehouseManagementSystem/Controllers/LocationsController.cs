ee5dc607b1fd15ed27ff7441680ffed9f4909be4
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2025-11-21 14:58:28 +0000

    Created the Locations Controller

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Controllers/LocationsController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagementSystem.Models;
+
+namespace WarehouseManagementSystem.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly WarehouseDbContext _context;
+
+        public LocationsController(WarehouseDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Locations
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Locations.ToListAsync());
+        }
+
+        // GET: Locations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locations/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LocationId,Name,Aisle,Shelf")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LocationId,Name,Aisle,Shelf")] Location location)
+        {
+            if (id != location.LocationId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(location.LocationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound("Location not found");
+            }
+            try
+            {
+                _context.Locations.Remove(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+                TempData["ErrorMessage"] = "Cannot delete location because it has related products.";
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.LocationId == id);
+        }
+    }
+}
