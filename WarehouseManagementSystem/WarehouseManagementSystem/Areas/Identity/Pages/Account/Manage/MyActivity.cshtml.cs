6c7ba1b6191e2aa20ab1d2f7dcc0d4121e8c2f43
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2025-11-21 00:08:49 +0000

    Added Static Notifications and Activities

--- WarehouseManagementSystem/WarehouseManagementSystem/Areas/Identity/Pages/Account/Manage/MyActivity.cshtml.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Areas/Identity/Pages/Account/Manage/MyActivity.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WarehouseManagementSystem.Areas.Identity.Pages.Account.Manage
 {
     public class MyActivityModel : PageModel
     {
+        public List<ActivityItem> Activities { get; set; }
+
         public void OnGet()
         {
+            Activities = new List<ActivityItem>
+            {
+                new ActivityItem("Login Successful", "bi-box-arrow-in-right", DateTime.Now.AddHours(-1)),
+                new ActivityItem("Password Changed", "bi-key", DateTime.Now.AddDays(-1)),
+                new ActivityItem("Profile Updated", "bi-person", DateTime.Now.AddDays(-2)),
+                new ActivityItem("2FA Enabled", "bi-shield-lock", DateTime.Now.AddDays(-4)),
+            };
         }
+
+        public record ActivityItem(string Action, string Icon, DateTime Timestamp);
     }
 }
