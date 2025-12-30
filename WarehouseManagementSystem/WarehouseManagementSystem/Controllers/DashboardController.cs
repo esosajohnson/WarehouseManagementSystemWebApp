5aee3bfe31468bfaafc9f7a83619764c5736a82d
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2025-12-30 21:23:35 +0000

    Further enhanced the entire Dashboard Page

--- WarehouseManagementSystem/WarehouseManagementSystem/Controllers/DashboardController.cs
+++ WarehouseManagementSystem/WarehouseManagementSystem/Controllers/DashboardController.cs
@@ -27,11 +27,24 @@ namespace WarehouseManagementSystem.Controllers
                 .Take(5)
                 .ToListAsync();
 
+            var RecentGoodsReceipts = await _context.GoodsReceipts
+                .Include(gr => gr.Supplier)
+                .OrderByDescending(gr => gr.ReceiptDate)
+                .Take(5)
+                .ToListAsync();
+
+            var RecentShipments = await _context.Shipments
+                .OrderByDescending(s => s.ShippingDate)
+                .Take(5)
+                .ToListAsync();
+
             ViewData["TotalProducts"] = totalProducts;
             ViewData["LowStock"] = lowStock;
             ViewData["TotalSuppliers"] = totalSuppliers;
             ViewData["TotalCategories"] = totalCategories;
             ViewData["RecentTransactions"] = recentTransactions;
+            ViewData["RecentGoodsReceipts"] = RecentGoodsReceipts;
+            ViewData["RecentShipments"] = RecentShipments;
 
             return View();
         }
