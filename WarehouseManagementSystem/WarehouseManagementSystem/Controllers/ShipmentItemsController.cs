0a4faf542576786952d516b82756f7c329ed9184
Esosa M. Johnson Ikponmwosa <esosajohnson93@gmail.com>
2026-05-07 00:57:57 +0100

    Add Shipments & ShipmentItems MVC CRUD
    Add full CRUD functionality for Shipments and ShipmentItems: new ShipmentsController and ShipmentItemsController with Index/Details/Create/Edit/Delete actions, view models binding, validation and concurrency handling. Also add Razor views (Index/Create/Details/Edit/Delete) for both entities. ShipmentItemsController uses EF Core Includes for related Product/Location/Shipment and SelectList-populated dropdowns; both controllers use WarehouseDbContext and redirect to Index after changes.

---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Controllers/ShipmentItemsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagementSystem.Models;
+
+namespace WarehouseManagementSystem.Controllers
+{
+    public class ShipmentItemsController : Controller
+    {
+        private readonly WarehouseDbContext _context;
+
+        public ShipmentItemsController(WarehouseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ShipmentItems
+        public async Task<IActionResult> Index()
+        {
+            var warehouseDbContext = _context.ShipmentItems.Include(s => s.Location).Include(s => s.Product).Include(s => s.Shipment);
+            return View(await warehouseDbContext.ToListAsync());
+        }
+
+        // GET: ShipmentItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipmentItem = await _context.ShipmentItems
+                .Include(s => s.Location)
+                .Include(s => s.Product)
+                .Include(s => s.Shipment)
+                .FirstOrDefaultAsync(m => m.ShipmentItemId == id);
+            if (shipmentItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipmentItem);
+        }
+
+        // GET: ShipmentItems/Create
+        public IActionResult Create()
+        {
+            ViewData["LocationId"] = new SelectList(_context.Locations, "LocationId", "LocationId");
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId");
+            ViewData["ShipmentId"] = new SelectList(_context.Shipments, "ShipmentId", "ShipmentId");
+            return View();
+        }
+
+        // POST: ShipmentItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ShipmentItemId,ShipmentId,ProductId,LocationId,QuantityShipped,UnitPrice,LineTotal")] ShipmentItem shipmentItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shipmentItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "LocationId", "LocationId", shipmentItem.LocationId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", shipmentItem.ProductId);
+            ViewData["ShipmentId"] = new SelectList(_context.Shipments, "ShipmentId", "ShipmentId", shipmentItem.ShipmentId);
+            return View(shipmentItem);
+        }
+
+        // GET: ShipmentItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipmentItem = await _context.ShipmentItems.FindAsync(id);
+            if (shipmentItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "LocationId", "LocationId", shipmentItem.LocationId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", shipmentItem.ProductId);
+            ViewData["ShipmentId"] = new SelectList(_context.Shipments, "ShipmentId", "ShipmentId", shipmentItem.ShipmentId);
+            return View(shipmentItem);
+        }
+
+        // POST: ShipmentItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ShipmentItemId,ShipmentId,ProductId,LocationId,QuantityShipped,UnitPrice,LineTotal")] ShipmentItem shipmentItem)
+        {
+            if (id != shipmentItem.ShipmentItemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shipmentItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShipmentItemExists(shipmentItem.ShipmentItemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LocationId"] = new SelectList(_context.Locations, "LocationId", "LocationId", shipmentItem.LocationId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "ProductId", "ProductId", shipmentItem.ProductId);
+            ViewData["ShipmentId"] = new SelectList(_context.Shipments, "ShipmentId", "ShipmentId", shipmentItem.ShipmentId);
+            return View(shipmentItem);
+        }
+
+        // GET: ShipmentItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipmentItem = await _context.ShipmentItems
+                .Include(s => s.Location)
+                .Include(s => s.Product)
+                .Include(s => s.Shipment)
+                .FirstOrDefaultAsync(m => m.ShipmentItemId == id);
+            if (shipmentItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipmentItem);
+        }
+
+        // POST: ShipmentItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shipmentItem = await _context.ShipmentItems.FindAsync(id);
+            if (shipmentItem != null)
+            {
+                _context.ShipmentItems.Remove(shipmentItem);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShipmentItemExists(int id)
+        {
+            return _context.ShipmentItems.Any(e => e.ShipmentItemId == id);
+        }
+    }
+}
---
+++ WarehouseManagementSystem/WarehouseManagementSystem/Controllers/ShipmentsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagementSystem.Models;
+
+namespace WarehouseManagementSystem.Controllers
+{
+    public class ShipmentsController : Controller
+    {
+        private readonly WarehouseDbContext _context;
+
+        public ShipmentsController(WarehouseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shipments
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Shipments.ToListAsync());
+        }
+
+        // GET: Shipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipments
+                .FirstOrDefaultAsync(m => m.ShipmentId == id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipment);
+        }
+
+        // GET: Shipments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shipments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ShipmentId,Carrier,TrackingNumber,ShippingDate,Destination,Status,Notes")] Shipment shipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+            return View(shipment);
+        }
+
+        // POST: Shipments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ShipmentId,Carrier,TrackingNumber,ShippingDate,Destination,Status,Notes")] Shipment shipment)
+        {
+            if (id != shipment.ShipmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShipmentExists(shipment.ShipmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipments
+                .FirstOrDefaultAsync(m => m.ShipmentId == id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipment);
+        }
+
+        // POST: Shipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment != null)
+            {
+                _context.Shipments.Remove(shipment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShipmentExists(int id)
+        {
+            return _context.Shipments.Any(e => e.ShipmentId == id);
+        }
+    }
+}
